bf5417d32b45e6fd6c0fb2745c6906256b73c9a6
Sean Sung <numbers633@gmail.com>
2016-12-14 16:59:03 -0800

    Add project files.

---
+++ Google Chrome Extension1/app/background.js
@@ -0,0 +1,28 @@
+﻿chrome.runtime.onConnect.addListener(function (port) {
+    console.assert(port.name == "Link Foresight");
+
+    port.onMessage.addListener(function (message) {
+        readURL(port, message);
+    });
+});
+
+function readURL(port, message) {
+    console.log(message);
+    var req = new XMLHttpRequest();
+    req.open('GET', message.url, false);
+    req.send();
+
+    console.log(req.status);
+
+    if (req.status == 200) {
+        var title = req.responseText.match(/<title[^>]*>([^<]+)<\/title>/)[1];
+        if (title == null) {
+            title = "N/A";
+        }
+        console.log(req.responseText);
+        port.postMessage({ Success: true, Title: title });
+    } else {
+        port.postMessage({ Success: false });
+        console.log(req.status);
+    }
+}
---
+++ Google Chrome Extension1/app/contentscript.js
@@ -0,0 +1,66 @@
+﻿var bubbleElement = document.createElement("div");
+bubbleElement.setAttribute("class", "triangle-border");
+document.body.appendChild(bubbleElement);
+
+var port = chrome.runtime.connect({name: "Link Foresight"});
+port.onMessage.addListener(function (message) {
+    showInfo(message);
+});
+
+//coordinates for bubbleElement
+var x = 0, y = 0;
+
+function getInfo(link) {
+    console.log(link.href);
+    port.postMessage({url: link.href});
+}
+
+function showInfo(message) {
+    console.log(message);
+    var bubbleX, bubbleY;
+
+    bubbleX = positionX();
+    bubbleY = positionY();
+
+    if (message.Success) {
+        bubbleElement.innerHTML = message.Title;
+    } else {
+        bubbleElement.innerHTML = "No information retrieved.";
+    }
+
+    bubbleElement.style.top = bubbleY + 'px';
+    bubbleElement.style.left = bubbleX + 'px';
+    bubbleElement.style.visibility = 'visible';
+}
+
+function hideInfo(message) {
+    bubbleElement.style.visibility = 'hidden';
+}
+
+function renderBubble(message) {
+   
+}
+
+function positionX() {
+    return (x - 100 > 0) ? x - 100 : x;
+}
+
+function positionY() {
+    return (y - 100 > 0) ? y - 100 : y;
+}
+
+for (var i = 0; i < document.links.length; i++) {
+    (function (i) {
+        document.links[i].addEventListener("contextmenu", function (e) {
+            //update coordinates on right click
+            x = e.pageX;
+            y = e.pageY;
+
+            getInfo(document.links[i]);
+        });
+
+        document.links[i].addEventListener("mouseleave", function () {
+            hideInfo(document.links[i]);
+        });
+    })(i);
+}
