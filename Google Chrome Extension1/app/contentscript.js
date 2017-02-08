990ab2735749455ab14a2eb9b66523fd1705c2d6
Sean Sung <numbers633@gmail.com>
2017-02-08 02:26:02 -0800

    Improved youtube details and visuals/formatting

--- Google Chrome Extension1/app/contentscript.js
+++ Google Chrome Extension1/app/contentscript.js
@@ -1,7 +1,21 @@
-﻿var bubbleElement = document.createElement("div");
-bubbleElement.setAttribute("class", "triangle-border");
+﻿//HTML bubble initialization
+var bubbleElement = document.createElement("div");
+var titleContent = document.createElement("div");
+var leftContent = document.createElement("div");
+var rightContent = document.createElement("div");
+
+bubbleElement.setAttribute("class", "standard-border");
+titleContent.setAttribute("class", "title-content");
+leftContent.setAttribute("class", "left-content");
+rightContent.setAttribute("class", "right-content");
+
+bubbleElement.appendChild(titleContent);
+bubbleElement.appendChild(leftContent);
+bubbleElement.appendChild(rightContent);
 document.body.appendChild(bubbleElement);
 
+
+//connection to background script
 var port = chrome.runtime.connect({name: "Link Foresight"});
 port.onMessage.addListener(function (message) {
     showInfo(message);
@@ -23,13 +37,7 @@ function showInfo(message) {
 
     if (message.Success) {
         if (message.linkType == "youtube") {
-            var likeCount = parseInt(message.data.items[0].statistics.likeCount);
-            var dislikeCount = parseInt(message.data.items[0].statistics.dislikeCount);
-            var ratio = Math.floor(100 * likeCount / (likeCount + dislikeCount));
-
-            bubbleElement.innerHTML = message.data.items[0].snippet.title + '<br>' +
-                'Views: ' + message.data.items[0].statistics.viewCount + '<br>' +
-                'Likes/Dislikes: ' + likeCount + '/' + dislikeCount + ' (' + ratio + '%)';
+            displayYoutubeBubble(message.data);
         } else {
             bubbleElement.innerHTML = message.Title;
         }
@@ -37,9 +45,31 @@ function showInfo(message) {
         bubbleElement.innerHTML = "No information retrieved.";
     }
 
-    bubbleElement.style.top = bubbleY + 'px';
-    bubbleElement.style.left = bubbleX + 'px';
     bubbleElement.style.visibility = 'visible';
+}
+
+/* Youtube handler function */
+function displayYoutubeBubble(data) {
+    var likeCount = parseInt(data.items[0].statistics.likeCount);
+    var dislikeCount = parseInt(data.items[0].statistics.dislikeCount);
+    var ratio = Math.floor(100 * likeCount / (likeCount + dislikeCount));
+    var views = parseInt(data.items[0].statistics.viewCount);
+
+    titleContent.innerHTML = '<b>' + data.items[0].snippet.title +
+        '</b><hr class="line-hr">';
+    leftContent.innerHTML = 'Uploader: ' +data.items[0].snippet.channelTitle + '<br>' +
+        'Date: ' + getDateString(data.items[0].snippet.publishedAt);
+    rightContent.innerHTML = 'Views: ' + views.toLocaleString() + '<br>' +
+        'Rating: ' + likeCount.toLocaleString() + '/' + dislikeCount.toLocaleString() + ' (' + ratio + '%)';
+}
+
+/* Function to parse DateTime string and return date without time */
+function getDateString(dateString) {
+    var date = new Date(dateString);
+    // convert 0-11 month value to 1-12
+    var month = (parseInt(date.getMonth()) + 1).toString();
+
+    return month + '/' + date.getDate() + '/' + date.getFullYear();
 }
 
 function hideInfo(message) {
